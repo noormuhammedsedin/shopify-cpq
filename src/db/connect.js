abf2585f2a766c9b8e996b3606b142ca81c6a9e6
Noor muhammed <noor.muhammed@coderapper.com>
2024-07-19 11:08:37 +0530

    mongo url changed

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-const mogoUrl=process.env.MONGO_URL
+const mogoUrl= "mongodb+srv://noormuhammed:d8TAwWKJ6KZObG3p@cluster0.vxwvvvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connect = async () => {
   try {
     await mongoose.connect(mogoUrl, {
